20329af45e59f420c795011a778123fd9cf8f09d
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2023-01-11 00:15:16 +0600

    my reviews added

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,30 +1,77 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../contexts/AuthProvider';
-import Review from './Review';
+import React, { useContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../contexts/AuthProvider";
+import Review from "./Review";
 
 const MyReviews = () => {
-    const { user } = useContext(AuthContext);
-    const [reviews, setReviews] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [reviews, setReviews] = useState([]);
   
-    useEffect(()=> {
-      fetch(`https://server-fawn-pi.vercel.app/reviews?email=${user?.email}`)
-      .then(res => res.json())
-      .then(data => {
+
+  const handleDeleteReview = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Do you want delete this review?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteOperation(id);
+      }
+    });
+  };
+
+  const deleteOperation = (id) => {
+    fetch(`http://localhost:5000/reviews/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        // authorization: `Bearer ${localStorage.getItem('hairCutToken')}`
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        if (data?.deletedCount) {
+          const remainingReviews = reviews.filter(
+            (review) => review._id !== id
+          );
+          setReviews(remainingReviews);
+          Swal.fire("Deleted!", "Your Review has been deleted.", "success");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetch(`https://server-fawn-pi.vercel.app/reviews?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
         setReviews(data);
-      })
-    },[user?.email])
+      });
+  }, [user?.email]);
 
-    return (
-        <div>
-          {
-            !reviews.length && <p>No reviews was found</p>
-          }
-           {
-            reviews?.map(review => <Review key={review._id} review={review}></Review>)
-           }
-        </div>
-    );
+  return (
+    <div>
+      {!reviews.length && <p>No reviews was found</p>}
+
+      {reviews?.map((review) => (
+        <Review
+          key={review._id}
+          review={review}
+          handleDeleteReview={handleDeleteReview}
+        ></Review>
+      ))}
+
+
+
+    </div>
+  );
 };
 
 export default MyReviews;
--- src/Pages/MyReviews/Review.js
+++ src/Pages/MyReviews/Review.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import defaultImg from "../../assets/images/person.png"
+import React from "react";
+import defaultImg from "../../assets/images/person.png";
 
-const Review = ({review}) => {
-    const { photoUrl, comment, reviewer_name,  reviewer_email } = review
-    return (
-        <div className="chat chat-start border-2 border-indigo-500 mb-3 rounded">
+const Review = ({ review, handleDeleteReview }) => {
+  const { photoUrl, comment, reviewer_name, reviewer_email, _id } = review;
+
+
+
+  return (
+    <div className="flex m-8">
+      <div className="chat chat-start border-2 border-indigo-500 mb-3 rounded w-full">
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            {
-             photoUrl ? <img
-              src={photoUrl}
-              alt="reviewImg"
-            /> : <img
-            src={defaultImg}
-            alt="reviewImg"
-          />
-            }
+            {photoUrl ? (
+              <img src={photoUrl} alt="reviewImg" />
+            ) : (
+              <img src={defaultImg} alt="reviewImg" />
+            )}
           </div>
         </div>
         <div className="">
-          <h3 className="font-bold text-2xl py-2"> {reviewer_name ? reviewer_name : reviewer_email }</h3>
+          <h3 className="font-bold text-2xl py-2">
+            {" "}
+            {reviewer_name ? reviewer_name : reviewer_email}
+          </h3>
           {comment}
         </div>
       </div>
-    );
+      &nbsp;
+      <div style={{ 'width': "200px", }}>
+        <button className="btn btn-info">Edit</button> &nbsp;
+        <button onClick={() => handleDeleteReview(_id)} className="btn btn-error">Delete</button>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
